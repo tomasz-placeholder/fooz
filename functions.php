f684d13b04b189db4b6b00d1e08e10dc90c802f5
Tomasz Frankowski <tomasz@placeholderstud.io>
2025-07-08 14:33:02 +0200

    Init
    Scaffold child theme using wp-cli
    
    `wp scaffold child-theme twentytwentyfive-child
    --parent_theme=twentytwentyfive --theme_name="Fooz" --author="Tomasz
    Frankowski" --activate`

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fooz Theme functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package twentytwentyfive-child
+ */
+
+add_action( 'wp_enqueue_scripts', 'twentytwentyfive_child_parent_theme_enqueue_styles' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function twentytwentyfive_child_parent_theme_enqueue_styles() {
+	wp_enqueue_style( 'twentytwentyfive-style', get_template_directory_uri() . '/style.css', array(), '0.1.0' );
+	wp_enqueue_style(
+		'fooz-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'twentytwentyfive-style' ),
+		'0.1.0'
+	);
+}
